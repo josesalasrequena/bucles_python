5452b3cb3b3285610848b3c460dfc0d49553992a
josesalasrequena <87277797+josesalasrequena@users.noreply.github.com>
2021-08-01 18:40:45 -0300

    Add files via upload

---
+++ ejercicio_1.py
@@ -0,0 +1,40 @@
+# Bucles [Python]
+# Ejercicios de práctica
+
+# Autor: Inove Coding School
+# Version: 2.0
+
+# IMPORTANTE: NO borrar los comentarios
+# que aparecen en verde con el hashtag "#"
+
+# Ejercicios con bucles "while"
+
+x = 0
+# Dado el siguiente "while", complete la condicion
+# para que el "while" itere siempre que <x sea menor a 6>
+# Además, complete la línea de código necesaria para que
+# el valor de "x" incremente "1" en cada iteración
+
+while x < 6:
+    print("Valor de x =", x)
+    x +=1   # Incrementar x en 1
+
+# reemplace "condicion" por lo que crea necesario
+
+    # Coloque la línea de código para que "X" incremente "1"
+
+
+x = 5
+# Dado el siguiente "while", complete la condicion
+# para que el "while" itere siempre que <x sea mayor o igual a 0>
+# Además, complete la línea de código necesaria para que
+# el valor de "x" decremente "1" en cada iteración
+
+while x >= 0:
+    print("Valor de x =", x)
+    x -=1   # Decremente x en 1
+
+# reemplace "condicion" por lo que crea necesario
+    # Coloque la línea de código para que "X" decremente "1"
+
+print("terminamos!")
